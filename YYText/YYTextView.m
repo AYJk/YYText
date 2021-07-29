9245da97b334384819cc12f409c8320a31dd2cc4
Lois <andy5759520@gmail.com>
2021-07-29 19:23:27 +0800

    修改源码为输入内容不跟随前一个富文本样式

--- YYText/YYTextView.m
+++ YYText/YYTextView.m
@@ -1468,7 +1468,10 @@ typedef NS_ENUM(NSUInteger, YYTextMoveDirection) {
     }
     if (notify) [_inputDelegate textWillChange:self];
     NSRange newRange = NSMakeRange(range.asRange.location, text.length);
-    [_innerText replaceCharactersInRange:range.asRange withString:text];
+    NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
+    style.lineSpacing = 5;
+    [_innerText replaceCharactersInRange:range.asRange withAttributedString:[[NSAttributedString alloc] initWithString:text attributes:@{NSForegroundColorAttributeName: [UIColor colorWithRed:51/255.0 green:51/255.0 blue:51/255.0 alpha:1], NSFontAttributeName: [UIFont systemFontOfSize:14], NSParagraphStyleAttributeName: style}]];
+//    [_innerText replaceCharactersInRange:range.asRange withString:text];
     [_innerText yy_removeDiscontinuousAttributesInRange:newRange];
     if (notify) [_inputDelegate textDidChange:self];
 }
